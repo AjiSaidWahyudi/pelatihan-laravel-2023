a865349000ffc8fec3a643b10f7d2f74cec9e9ff
AjiSaidWahyudi <49503563+AjiSaidWahyudi@users.noreply.github.com>
2023-03-13 15:54:14 +0800

    Fifth commit

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,10 @@ Route::get('/', function () {
 
 Route::resource('/book', BookController::class);
 Route::post('/book/{id}/addstock', [BookController::class, 'addStock'])->name('book.addStock');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
